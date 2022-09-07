1648f61aecd78f91a0d397e28fc20ebfaf37ea18
nolste <nol.steiner@gmail.com>
2022-09-07 12:11:34 -0500

    changed parse, added printall

--- JYM1classRoster/roster.cpp
+++ JYM1classRoster/roster.cpp
@@ -1,17 +1,16 @@
 #include "roster.h"
 #include "student.h"
 
-const std::string studentData[] = {
-"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
-"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
-"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
-"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-"A5,Nolan,Steiner,jsteiner@wgu.edu,30, 3,2,1,SOFTWARE"
-};
 
 
+void Roster::add(string studentID, string firstName, string lastName, string emailAddress, int age,
+	int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeprogram) {
 
-Student* Roster::parse(string data) {
+	classRosterArray[++arrayIndex] = new Student(studentID, firstName, lastName, emailAddress, age, daysInCourse1, daysInCourse2, daysInCourse3, degreeprogram);
+}
+
+
+void Roster::parse(string data) {
 	size_t rhs = data.find(",");
 	string studentID = data.substr(0, rhs);
 
@@ -60,16 +59,29 @@ Student* Roster::parse(string data) {
 	else if (degreeProgram == "SOFTWARE") {
 		degree = DegreeProgram::SOFTWARE;
 	}
-	return new Student(studentID, firstName, lastName, email, age, numofdays1, numofdays2, numofdays3, degree);
+	Roster::add(studentID, firstName, lastName, email, age, numofdays1, numofdays2, numofdays3, degree);
+
 	
 
 
 
 }
 
-Student* Roster::add(string studentID, string firstName, string lastName, string emailAddress, int age,
-	int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeprogram) {
+void Roster::printAll() {
+	for (int i = 0; i <= arrayIndex; i++) {
+		std::cout << classRosterArray[i]->getStudentID() << '\t';
+		std::cout << classRosterArray[i]->getFirstName() << '\t';
+		std::cout << classRosterArray[i]->getLastName() << '\t';
+		std::cout << classRosterArray[i]->getEmailAddress() << '\t';
+		std::cout << classRosterArray[i]->getAge() << '\t';
+		std::cout << classRosterArray[i]->getNumberofDays() << '\t';
+		std::cout << classRosterArray[i]->getDegreeProgram() << '\t' << std::endl;
 
-	return new Student(studentID, firstName, lastName, emailAddress, age, daysInCourse1, daysInCourse2, daysInCourse3, degreeprogram);
+
+
+	}
 }
+
+
+
 
--- JYM1classRoster/roster.h
+++ JYM1classRoster/roster.h
@@ -16,12 +16,15 @@ public:
 	//const static int numStudents = 5;
 	//methods
 	//int getNumStudents();
-	static Student* parse(string);
-	static Student* add(string studentID, string firstName, string lastName, string emailAddress,
+	int arrayIndex = -1;
+	Student* classRosterArray[5]{};
+	void parse(string);
+	void add(string studentID, string firstName, string lastName, string emailAddress,
 		int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeprogram);
+
 	//void add();
 	//void remove(string studentID);
-	//void printAll();
+	void printAll();
 	//void printAverageDaysInCourse(string studentID);
 	//void printInvalidEmails();
 
